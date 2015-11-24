980d65b542b858db441b0a49a73ae90600d044fa
Curt Nichols <cnichols@soundmetrics.com>
2015-11-24 12:59:30 -0800

    Add frame functions

---
+++ common-code/FrameFuncs.h
@@ -0,0 +1,11 @@
+// FrameFuncs.h
+
+#include <stdint.h>
+
+#ifndef FRAMES_H
+#define FRAMES_H
+
+// Returns the number of beams used for the specified pingmode.
+size_t get_beams_from_pingmode(uint32_t pingmode);
+
+#endif // !FRAMES_H
