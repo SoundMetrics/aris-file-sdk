2a6cf4dff59bd7461d3bc028636edc1e1ed54676
Curt Nichols <cnichols@soundmetrics.com>
2015-12-24 09:46:58 -0800

    Debug output for the build

--- sdk-build-tools/build-packages-teamcity.cmd
+++ sdk-build-tools/build-packages-teamcity.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-REM TeamCity chokes on the COMPONENTSVERSION processing here, it thinks
-REM COMPONENTSVERSION is required going into the build step when we would
+REM TeamCity chokes on the COMPONENTS_VERSION processing here, it thinks
+REM COMPONENTS_VERSION is required going into the build step when we would
 REM define it in the build step.
 
 SETLOCAL
@@ -14,16 +14,19 @@ IF "%1" == "" GOTO Usage
 
 
 SET BUILD_NUMBER=%1
-
+ECHO BUILD_NUMBER=%BUILD_NUMBER%
 
 REM ---------------------------------------------------------------------------
 REM Build packages
 REM ---------------------------------------------------------------------------
 
-SET /P COMPONENTSVERSION=<ver.txt
-ECHO COMPONENTSVERSION=%COMPONENTSVERSION%
+SET /P COMPONENTS_VERSION=<ver.txt
+ECHO COMPONENTS_VERSION=%COMPONENTS_VERSION%
 
-call build-packages.cmd %COMPONENTSVERSION%.%BUILD_NUMBER%
+SET BUILD_VERSION=%COMPONENTS_VERSION%.%BUILD_NUMBER%
+ECHO BUILD_VERSION=%BUILD_VERSION%
+
+call build-packages.cmd %BUILD_VERSION%
 
 GOTO :EOF
 
