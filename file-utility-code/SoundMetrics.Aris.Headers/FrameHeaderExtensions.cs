e55ea52e66eabd28511862377f163abb1f413ace
Curt Nichols <curt.nichols@live.com>
2020-01-31 11:07:54 -0800

    Add a function for grabbing the correct beam count from ArisFrameHeader

---
+++ file-utility-code/SoundMetrics.Aris.Headers/FrameHeaderExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2015-2018 Sound Metrics Corp. All Rights Reserved.
+
+using System.Collections.Generic;
+
+namespace Aris.FileTypes
+{
+    /// <summary>
+    /// Extensions for type FrameHeader.
+    /// </summary>
+    public static class FrameHeaderExtensions
+    {
+        /// <summary>
+        /// Determines the beam count from the frame header.
+        /// </summary>
+        /// <param name="frameHeader">
+        /// The frame header of interest.
+        /// </param>
+        /// <returns>The beam count.</returns>
+        public static uint? GetBeamCount(this in ArisFrameHeader frameHeader)
+        {
+            var pingMode = frameHeader.PingMode;
+
+            if (PingModeBeamCounts.ContainsKey(pingMode))
+            {
+                return PingModeBeamCounts[pingMode];
+            }
+
+            return null;
+        }
+
+        private static readonly Dictionary<uint, uint> PingModeBeamCounts =
+            new Dictionary<uint, uint>
+            {
+                { 1, 48 },
+                { 3, 96 },
+                { 6, 64 },
+                { 9, 128 },
+            };
+    }
+}
